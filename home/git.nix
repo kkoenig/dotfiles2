9cc4cf00cad6e540952107892b6cc5a1d80aa553
Killian Koenig <killiankoenig@gmail.com>
2025-07-25 14:04:34 -0700

    Configure theme and default ai model

--- home/git.nix
+++ home/git.nix
@@ -3,22 +3,47 @@
     enable = true;
     userName = "Killian Koenig";
     userEmail = "killiankoenig@gmail.com";
+
     delta = {
       enable = true;
       options = {
         side-by-side = true;
         line-numbers = true;
+        navigate = true;
+        light = false;
       };
     };
+
     ignores = [
       ".direnv"
+      ".DS_Store"
+      "*.swp"
+      "*.swo"
+      "*~"
     ];
+
     extraConfig = {
+      init = {
+        defaultBranch = "main";
+      };
+      core = {
+        editor = "vim";
+        autocrlf = false;
+      };
       diff = {
         tool = "vimdiff";
       };
       difftool = {
         prompt = false;
+      };
+      pull = {
+        rebase = true;
+      };
+      push = {
+        autoSetupRemote = true;
+      };
+      branch = {
+        autoSetupRebase = "always";
       };
     };
   };
--- home/zed.nix
+++ home/zed.nix
@@ -4,6 +4,8 @@
 
     extensions = [
       "nix"
+      "catppuccin"
+      "catppuccin-icons"
     ];
 
     userSettings = {
@@ -13,6 +15,19 @@
       vim_mode = true;
       ui_font_size = 16;
       buffer_font_size = 16;
+      theme = {
+        light = "Catppuccin Mocha";
+        dark = "Catppuccin Mocha";
+        mode = "system";
+      };
+      icon_theme = "Catppuccin Mocha";
+
+      agent = {
+        default_model = {
+          provider = "anthropic";
+          model = "claude-sonnet-4-latest";
+        };
+      };
     };
 
     # Enable when formatting issue is fixed
